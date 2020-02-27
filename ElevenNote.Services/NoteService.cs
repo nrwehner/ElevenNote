cec82ae4a377a6cfe0d434b33fa74c667ab4dedf
Nick Wehner <nrwehner@gmail.com>
2020-02-27 12:18:37 -0500

    commit1218pm

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //public bool CreateNote(NoteCreate model)
+        //{
+
+        //}
+    }
+}
